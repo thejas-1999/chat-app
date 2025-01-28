27bf0e0cb604c18b35aba8068b9484b6dff50660
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-28 13:54:50 +0530

    create the conversation model

---
+++ backend/models/conversationModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    messages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+        default: [],
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ConverSation = mongoose.model("Conversation", conversationSchema);
+
+export default ConverSation;
